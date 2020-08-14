fabd5fa8b9e6fda792de8ed52f23171c55d59908
David Himmelstrup <lemmih@gmail.com>
2020-08-14 15:03:50 +0800

    Basic test scaffold using tasty and quickcheck.

--- test/MyLibTest.hs
+++ test/MyLibTest.hs
@@ -1,4 +1,20 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Main (main) where
 
+import           Data.Foldable
+import           Test.QuickCheck
+import           Test.Tasty
+import           Test.Tasty.QuickCheck
+
+import           Data.CompactSequence.Stack.Simple
+
+
+prop_identity lst = toList (fromList lst) == lst
+
+
+return [] -- This makes sure the above properties are seen by $allProperties
+all_props :: TestTree
+all_props = testProperties "properties" $allProperties
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = defaultMain all_props
