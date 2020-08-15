362b0befd313dfd41d16bb9b48a8bd42eef4b3a8
David Feuer <David.Feuer@gmail.com>
2020-08-15 01:44:18 -0400

    Lots of optimizing
    * I had to push some appending and splitting into `shiftA` to
      avoid unfortunate thunks.
    
    * The optimizer didn't handle my pattern synonym at all well
      in `shiftA`. Write it out all manually. Ouch.
    
    * Get rid of now-unnecessary `seq` calls.

--- src/Data/CompactSequence/Queue/Internal.hs
+++ src/Data/CompactSequence/Queue/Internal.hs
@@ -5,12 +5,10 @@
 {-# language PatternSynonyms #-}
 {-# language ViewPatterns #-}
 {-# language LambdaCase #-}
--- {-# OPTIONS_GHC -Wall #-}
+{- OPTIONS_GHC -Wall #-}
 {- OPTIONS_GHC -ddump-simpl #-}
 
 module Data.CompactSequence.Queue.Internal where
---import Data.Primitive.SmallArray (SmallArray)
---import qualified Data.Primitive.SmallArray as A
 import qualified Data.CompactSequence.Internal.Array as A
 import Data.CompactSequence.Internal.Array (Array, Size, Mult (..))
 import qualified Data.Foldable as F
@@ -20,15 +18,42 @@ data FD n a
   = FD1 !(Array n a)
   | FD2 !(Array n a) !(Array n a)
   | FD3 !(Array n a) !(Array n a) !(Array n a)
-  deriving (Functor, Foldable, Traversable)
 -- FD2 and FD3 are safe; FD1 is dangerous.
 
 data RD n a
   = RD0
   | RD1 !(Array n a)
   | RD2 !(Array n a) !(Array n a)
-  deriving (Functor, Foldable, Traversable)
 -- RD0 and RD1 are safe; RD2 is dangerous.
+
+-- Conceptually, we want something like
+--
+--   data Queue n a = Empty | Node !(FD n a) (Queue n a) !(RD n a)
+--
+-- Unfortunately, this is rather wasteful. The Node itself takes
+-- 4 words, and the digits combined take between 2 and 7. Total:
+-- between 6 and 11 words. By manually "unpacking" the digits, expanding
+-- the Queue to 10 constructors, we now have nodes taking
+-- between 3 and 7 words, a considerable improvement. This kind of
+-- unpacking, in general, can risk a loss of sharing, leading to
+-- increased allocation and (in the presence of persistence) increased
+-- residency. But that doesn't happen here! The worst case for the
+-- unpacked representation relative to the conceptual one is when
+-- the frost digit is 3 and we modify the rear digit. In that case,
+-- we have to copy the three front array pointers rather than a single
+-- front digit pointer. Consider, for example, changing a 0 digit
+-- to a 1 digit in the rear. For the conceptual representation, that
+-- allocates 4 words for the new Node plus 2 words for the new rear
+-- digit, for a total of 6 words. The unpacked representation
+-- allocates one word for the new node header, three words to copy the
+-- front, one word to copy the middle, and one word for the new rear.
+-- Total: 6. So in the case that's *worst* for the unpacked version,
+-- the unpacked version still breaks even in allocation, while
+-- winning the indirection game. So unpacked is the way to go.
+-- As long as we're doing it this way, we can bake the "no debits
+-- on children of unsafe nodes" invariant right into the constructors,
+-- preventing us from messing that up and as a side benefit avoiding
+-- some double forcing.
 
 data Queue n a
   = Empty
@@ -41,7 +66,7 @@ data Queue n a
   | Node30 !(Array n a) !(Array n a) !(Array n a) (Queue ('Twice n) a)
   | Node31 !(Array n a) !(Array n a) !(Array n a) (Queue ('Twice n) a) !(Array n a)
   | Node32 !(Array n a) !(Array n a) !(Array n a) !(Queue ('Twice n) a) !(Array n a) !(Array n a)
-  deriving (Functor, Traversable)
+  deriving (Functor, Foldable, Traversable)
 -- An Empty node is safe.
 -- A Node node is safe if both its digits are safe. We require that the child queue of an unsafe
 -- node be in WHNF, and allow no debits on it.
@@ -63,12 +88,18 @@ data Queue n a
 --
 -- We allow the child queue of a safe node four times its safety value (for some value of four).
 
+
+
+-- Gunk to define a `Node` pattern synonym to pretend we
+-- have real digits. Sadly, where we really want this most,
+-- it throws GHC's optimizer for a loop and it makes garbage
+-- code.
 data Queue_ n a
   = Empty_
   | Node_ !(FD n a) (Queue ('Twice n) a) !(RD n a)
 
 matchNode :: Queue n a -> Queue_ n a
-matchNode = \case
+matchNode q = case q of
   Empty -> Empty_
   Node10 x m -> Node_ (FD1 x) m RD0
   Node11 x m a -> Node_ (FD1 x) m (RD1 a)
@@ -111,12 +142,11 @@ viewA :: Size n -> Queue n a -> ViewA n a
 -- Non-cascading
 viewA !_ Empty = EmptyA
 viewA !_ (Node (FD3 sa1 sa2 sa3) m sf) = ConsA sa1 $ Node (FD2 sa2 sa3) m sf
-viewA !_ (Node (FD2 sa1 sa2) m sf) = ConsA sa1 $ m `seq` Node (FD1 sa2) m sf
+viewA !_ (Node (FD2 sa1 sa2) m sf) = ConsA sa1 $ Node (FD1 sa2) m sf
 -- Potentially cascading
 viewA !n (Node (FD1 sa1) m (RD2 sa2 sa3)) = ConsA sa1 $
-  case shiftA (A.twice n) m (A.append n sa2 sa3) of
-    ShiftedA sam m'
-      | (sam1, sam2) <- A.splitArray n sam
+  case shiftA n m sa2 sa3 of
+    ShiftedA sam1 sam2 m'
       -> Node (FD2 sam1 sam2) m' RD0
 viewA !n (Node (FD1 sa1) m sf) = ConsA sa1 $
   case viewA (A.twice n) m of
@@ -127,15 +157,6 @@ viewA !n (Node (FD1 sa1) m sf) = ConsA sa1 $
     ConsA sam m'
       | (sam1, sam2) <- A.splitArray n sam
       -> Node (FD2 sam1 sam2) m' sf
-
-{-
-viewA2 :: Size n -> Queue n a -> ViewA2 n a
-viewA2 n q = case viewA n q of
-  EmptyA -> EmptyA2
-  ConsA sa q'
-    | (sa1, sa2) <- A.splitArray n sa
-    -> ConsA2 sa1 sa2 q'
--}
 
 empty :: Queue n a
 empty = Empty
@@ -157,14 +178,14 @@ perform all of these eagerly.
 
 snocA :: Size n -> Queue n a -> Array n a -> Queue n a
 snocA !_ Empty sa = Node (FD1 sa) empty RD0
-snocA !_ (Node pr m RD0) sa = Node pr m (RD1 sa)
-snocA !_ (Node pr m (RD1 sa1)) sa2 = m `seq` Node pr m (RD2 sa1 sa2)
 snocA !n (Node (FD1 sa0) m (RD2 sa1 sa2)) sa3
-  | ShiftedA sam m' <- shiftA (A.twice n) m (A.append n sa1 sa2)
-  , (sam1, sam2) <- A.splitArray n sam
+  | ShiftedA sam1 sam2 m' <- shiftA n m sa1 sa2
   = Node (FD3 sa0 sam1 sam2) m' (RD1 sa3)
+snocA !_ (Node pr m RD0) sa = Node pr m (RD1 sa)
+snocA !_ (Node pr m (RD1 sa1)) sa2 = Node pr m (RD2 sa1 sa2)
 snocA !n (Node pr m (RD2 sa1 sa2)) sa3
   = Node pr (snocA (A.twice n) m (A.append n sa1 sa2)) (RD1 sa3)
+
 
 -- | Uncons from a node and snoc onto it. Ensure that if the operation is
 -- expensive then it leaves the node in a safe configuration. Why do we need
@@ -198,58 +219,63 @@ snocA !n (Node pr m (RD2 sa1 sa2)) sa3
 --
 -- we have to do the opposite: snoc then view. We might as well
 -- just write a dedicated shifting operation.
-shiftA :: Size n -> Queue n a -> Array n a -> ShiftedA n a
+shiftA :: Size n -> Queue ('Twice n) a -> Array n a -> Array n a -> ShiftedA n a
+
+-- BLAST AND DARN. I started out using the Node pattern synonym all
+-- through here. Sadly, GHC was *way* too eager with join point
+-- transformations and decided to actually pass around front
+-- and rear digits to make things slow. GRRR. So in this function,
+-- we use the raw constructors by hand.
+
 -- Non-cascading cases
-shiftA !_ Empty sa = ShiftedA sa Empty
-shiftA !_ (Node (FD2 sa1 sa2) m RD0) sa3
-  = ShiftedA sa1 $ m `seq` Node (FD1 sa2) m (RD1 sa3)
-shiftA !_ (Node (FD2 sa1 sa2) m (RD1 sa3)) sa4
-  = ShiftedA sa1 $ m `seq` Node (FD1 sa2) m (RD2 sa3 sa4)
-shiftA !_ (Node (FD3 sa1 sa2 sa3) m RD0) sa4
-  = ShiftedA sa1 $ Node (FD2 sa2 sa3) m (RD1 sa4)
-shiftA !_ (Node (FD3 sa1 sa2 sa3) m (RD1 sa4)) sa5
-  = ShiftedA sa1 $ m `seq` Node (FD2 sa2 sa3) m (RD2 sa4 sa5)
+shiftA !_ Empty !sa1 !sa2 = ShiftedA sa1 sa2 Empty
+shiftA !n (Node20 sa1 sa2 m) !sa3 !sa4
+  = shrift n sa1 $ Node11 sa2 m (A.append n sa3 sa4)
+shiftA !n (Node21 sa1 sa2 m  sa3) !sa4 !sa5
+  = shrift n sa1 $ Node12 sa2 m sa3 (A.append n sa4 sa5)
+shiftA !n (Node30 sa1 sa2 sa3 m) !sa4 !sa5
+  = shrift n sa1 $ Node21 sa2 sa3 m (A.append n sa4 sa5)
+shiftA !n (Node31 sa1 sa2 sa3 m sa4) !sa5 !sa6
+  = shrift n sa1 $ Node22 sa2 sa3 m sa4 (A.append n sa5 sa6)
 -- cascading cases
-shiftA !n (Node (FD1 sa1) m RD0) sa3
-  = ShiftedA sa1 $
-      case viewA (A.twice n) m of
-        EmptyA -> singletonA sa3
+shiftA !n (Node10 sa1 m) !sa3 !sa4
+  = shrift n sa1 $
+      case viewA (A.twice (A.twice n)) m of
+        EmptyA -> Node10 (A.append n sa3 sa4) Empty
         ConsA sam m'
-          | (sam1, sam2) <- A.splitArray n sam
-          -> Node (FD2 sam1 sam2) m' (RD1 sa3)
-shiftA !n (Node (FD1 sa1) m (RD1 sa2)) sa3
+          | (sam1, sam2) <- A.splitArray (A.twice n) sam
+          -> Node21 sam1 sam2 m' (A.append n sa3 sa4)
+shiftA !n (Node11 sa1 m sa2) !sa3 !sa4
     -- We force sa3 here to avoid forming a chain of thunks if
     -- we have a bunch of FD1+RD1 nodes in a row.
-  = ShiftedA sa1 $ sa3 `seq`
-      case shiftA (A.twice n) m (A.append n sa2 sa3) of
-        ShiftedA sam m'
-          | (sam1, sam2) <- A.splitArray n sam
-          -> Node (FD2 sam1 sam2) m' RD0
-shiftA n (Node (FD1 sa1) m (RD2 sa2 sa3)) sa4
-  = ShiftedA sa1 $
-      case shiftA (A.twice n) m (A.append n sa2 sa3) of
-        ShiftedA sam m'
-          | (sam1, sam2) <- A.splitArray n sam
-          -> Node (FD2 sam1 sam2) m' (RD1 sa4)
-shiftA n (Node (FD2 sa1 sa2) m (RD2 sa3 sa4)) sa5
-  = ShiftedA sa1 $
-      case shiftA (A.twice n) m (A.append n sa3 sa4) of
-        ShiftedA sam m'
-          | (sam1, sam2) <- A.splitArray n sam
-          -> Node (FD3 sa2 sam1 sam2) m' (RD1 sa5)
-shiftA n (Node (FD3 sa1 sa2 sa3) m (RD2 sa4 sa5)) sa6
-  = ShiftedA sa1 $ Node (FD2 sa2 sa3) (snocA (A.twice n) m (A.append n sa4 sa5)) (RD1 sa6)
+  = shrift n sa1 $
+      case shiftA (A.twice n) m sa2 (A.append n sa3 sa4) of
+        ShiftedA sam1 sam2 m'
+          -> Node20 sam1 sam2 m'
+shiftA n (Node12 sa1 m sa2 sa3) !sa4 !sa5
+  = shrift n sa1 $
+      case shiftA (A.twice n) m sa2 sa3 of
+        ShiftedA sam1 sam2 m'
+          -> Node21 sam1 sam2 m' (A.append n sa4 sa5)
+shiftA n (Node22 sa1 sa2 m sa3 sa4) !sa5 !sa6
+  = shrift n sa1 $
+      case shiftA (A.twice n) m sa3 sa4 of
+        ShiftedA sam1 sam2 m'
+          -> Node31 sa2 sam1 sam2 m' (A.append n sa5 sa6)
+shiftA n (Node32 sa1 sa2 sa3 m sa4 sa5) !sa6 !sa7
+  = shrift n sa1 $
+      Node21 sa2 sa3
+           (snocA (A.twice (A.twice n)) m (A.append (A.twice n) sa4 sa5))
+           (A.append n sa6 sa7)
 
-data ShiftedA n a = ShiftedA !(Array n a) (Queue n a)
+shrift :: Size n -> Array ('Twice n) a -> Queue ('Twice n) a -> ShiftedA n a
+shrift n sa q
+  | (sa1, sa2) <- A.splitArray n sa
+  = ShiftedA sa1 sa2 q
+{-# INLINE shrift #-}
 
-{-
-splitArray :: SmallArray a -> (SmallArray a, SmallArray a)
-splitArray sa1 = (sa2, sa3)
-  where
-    !len' = A.sizeofSmallArray sa1 `quot` 2
-    !sa2 = A.cloneSmallArray sa1 0 len'
-    !sa3 = A.cloneSmallArray sa1 len' len'
--}
+
+data ShiftedA n a = ShiftedA !(Array n a) !(Array n a) (Queue ('Twice n) a)
 
 instance Show a => Show (Queue n a) where
     showsPrec p xs = showParen (p > 10) $
@@ -260,14 +286,3 @@ instance Eq a => Eq (Queue n a) where
 
 instance Ord a => Ord (Queue n a) where
   compare = compare `on` F.toList
-
-instance Foldable (Queue n) where
-  foldMap _f Empty = mempty
-  foldMap f (Node pr m sf) = foldMap f pr <> foldMap f m <> foldMap f sf
-
-  null Empty = True
-  null _ = False
-
-  -- TODO: Once the size type has really stabilized,
-  -- we should find a way to write a custom length.
-  -- Until then, we leave that to the wrapper implementation.
